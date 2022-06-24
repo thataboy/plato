1511a8057a76f25fff94c419211040cfa051c2fb
Bastien Dejean <nihilhill@gmail.com>
2022-06-24 09:32:04 +0200

    Remove debugging leftover
    It appears that b3bf054d304e1386c8ff94c689cc7db443b525a3 that fixes
    https://www.mobileread.com/forums/showpost.php?p=4188049&postcount=814
    contains a debugging leftover.
    
    Fixes #245.

--- src/document/html/engine.rs
+++ src/document/html/engine.rs
@@ -737,7 +737,7 @@ impl Engine {
                     style: parent_style.clone(),
                 }));
             },
-            NodeData::Whitespace(TextData { offset, text }) if parent_style.retain_whitespace => {
+            NodeData::Whitespace(TextData { offset, text }) => {
                 inlines.push(InlineMaterial::Text(TextMaterial {
                     offset: *offset,
                     text: text.to_string(),
